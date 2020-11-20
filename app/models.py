6660fcde6b670746ffcb178e65a8963732db0d32
Desar Mejdani <mejdanid@gmail.com>
2020-11-20 01:08:16 +0100

    Adding receipt & item models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db, login_manager
 from flask_login import UserMixin
+from datetime import datetime
 
 
 @login_manager.user_loader
@@ -14,3 +15,27 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}')"
+
+
+class Receipt(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    shop = db.Column(db.String(20), nullable=False)
+    cost = db.Column(db.Float, nullable=False)
+    payer = db.Column(db.String(10), nullable=True)
+    location = db.Column(db.String(40), nullable=True)
+    posts = db.relationship("Item", backref="receipt", lazy=True)
+
+    def __repr__(self):
+        return f"Receipt({self.date}, {self.shop}, {self.cost})"
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Float, nullable=False)
+    unit = db.Column(db.String, nullable=False)
+    receipt_id = db.Column(db.Integer, db.ForeignKey('receipt.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Item({self.name}, {self.price}, receipt={self.receipt_id})"
--- app/users/routes.py
+++ app/users/routes.py
@@ -14,10 +14,6 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        print(user.password)
-        print(type(user.password))
-        print(form.password.data)
-        print(type(form.password.data))
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
             next_page = request.args.get("next")
