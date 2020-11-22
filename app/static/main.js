bbcd277ea540a9dc17addd1505cc5f40836969be
Desar Mejdani <mejdanid@gmail.com>
2020-11-22 21:38:50 +0100

    Receipt route improvements

--- app/static/main.js
+++ app/static/main.js
@@ -1,6 +1,17 @@
-document.getElementById("itemadd").addEventListener("click", displayDate);
+document.getElementById("itemadd").addEventListener("click", add_item);
+document.getElementById("itempop").addEventListener("click", pop_item);
+document.getElementById('receiptdate').valueAsDate = new Date();
 
-function displayDate() {
-    jQuery("#itemform").clone().appendTo("#bodyreceipt")
-}
+var items = 1;
 
+function add_item() {
+    jQuery("#itemform").clone().appendTo("#bodyreceipt").attr('id', 'item_' + items);
+    items = items + 1;
+};
+
+function pop_item() {
+    if (items > 1) {
+        items = items - 1;
+        jQuery("#item_" + items).remove();
+    }
+};
